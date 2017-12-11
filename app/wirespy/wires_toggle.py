ca7fa68ab91ae8d8e71e2d7ea236dbd945558094
Shannon Glavin <sglavin@sas.upenn.edu>
2017-12-11 13:33:43 -0500

    added a log-writer to wires.py and a createlog option to wires_toggle.py

--- app/wirespy/wires_toggle.py
+++ app/wirespy/wires_toggle.py
@@ -10,10 +10,10 @@ Description: features of wires.py to toggle
 locallaptop = 0 #1=shannon's working on local laptop, 0=not that
 
 plotEach = 0    #1=plot each individual track, 0=only plot visual heat maps 
-badarealimit = 200000000000 #limit of triangular area for track to be included
+badarealimit = 2000 #38841 #limit of triangular area for track to be included
 
 #plot style options
-plt_dots = 0
+plt_dots = 1
 plt_lines = 0
 plt_shading = 1
 
@@ -21,3 +21,5 @@ plt_shading = 1
 returnoption = 2 #0=polyregion_all, 1=triareas_all, 2=badevents
 
 showprogress = 0 #1=print out percent done with wiresMain, 0=don't
+
+createlog = 1 #1=create a text file with info, 0=don't
